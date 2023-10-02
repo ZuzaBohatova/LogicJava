27264a37a541844b83c806ccaedccd83ab0c7a52
Zuzana Bohatová <101879462+ZuzaBohatova@users.noreply.github.com>
2023-10-02 22:47:35 +0200

    Delete logic/type-search-index.js

--- logic/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"logic","l":"Game"},{"p":"logic","l":"GamePlan"},{"p":"logic","l":"Machine"},{"p":"logic","l":"Main"},{"p":"logic","l":"User"}];updateSearchResults();
