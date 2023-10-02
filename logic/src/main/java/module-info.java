64c12fcb34e77c83034c721d04fbe788806e6d76
Zuzana Bohatová <101879462+ZuzaBohatova@users.noreply.github.com>
2023-10-02 09:33:12 +0200

    Add files via upload

---
+++ logic/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Program Bohatova_Logic implemetuje hru Logic (MasterMind) s dodaným vylepšením jako možnost vybrat si délku hádaného kódu,
+ * vybrat nejvyšší číslo v kódu, maximální počet kroků k uhádnutí, nebo využít nápovědu.
+ *
+ * @author Zuzana Bohatová
+ * @since 2023-05-05
+ */
+module logic {}
